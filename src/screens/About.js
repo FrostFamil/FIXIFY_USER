0d8fc76dad44e2e935e4ddbfe564d6d771cc4498
Famil <frostgamingrespawn@gmail.com>
2020-05-07 18:04:41 +0200

    updated

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import {View,Text} from "react-native";
-import {Header, Left, Icon, Body, Title, Footer} from 'native-base';
+import {View,Text, Image} from "react-native";
+import {Header, Left, Icon, Body, Title} from 'native-base';
 import { CardSection } from '../components';
 import {Entypo} from '@expo/vector-icons';
 
@@ -30,13 +30,21 @@ export default class About extends Component {
             <Title style={{ color: 'white'}}>About</Title>
           </Body>
         </Header>
+
+        <View style={{ alignItems: 'center', marginTop: 10}}>
+          <Image 
+            source={require('../../assets/UsedPhotos/Information.jpg')}
+            style={{ width: 200, height: 200, borderRadius: 20}}
+          />
+        </View>
         
         <CardSection>
         <Text
           {...this.props} // Inherit any props passed to it; e.g., multiline, numberOfLines below
-          style={{ fontSize: 18 }}
+          style={{ fontSize: 20 }}
           multiline = {true}
-        >Here Will Be Text About Us</Text>
+        >FIXIFY is a service application which aims to ease ordering of repairers. With the help of this application users will able to order fixers almost for any services.
+        Also, fixers will also able to get into technological world where they can able to access and accept projects from anywhere easily with only using their phones.</Text>
        </CardSection>
 
       </View>
